6ec9bd698d3a09803a0e9973e02d7c86da628442
thoma-achan <76838413+thoma-achan@users.noreply.github.com>
2024-12-30 14:34:32 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,25 +5,29 @@
         const message = document.getElementById('message');
 
         try {
-            const response = await fetch('Password.txt');
-            if (!response.ok) throw new Error('Could not load passwords file.');
-
-            const text = await response.text();
-            const validPasswords = text.split('\n').map(pw => pw.trim());
-
-            if (validPasswords.includes(password)) {
-                localStorage.setItem('authenticated', 'true');
-                localStorage.setItem('username', username);
-                if (username === 'ADMIN') {
-                    localStorage.setItem('role', 'admin');
-                } else {
-                    localStorage.setItem('role', username);
-                }
-               window.location.href = 'index_1.html';
-            } else {
-                message.textContent = 'Invalid password.';
-                message.style.color = 'red';
+          localStorage.setItem('username', username);
+          if (username === 'ADMIN') {
+              localStorage.setItem('role', 'admin');
+              const response = await fetch('AdminPassword.txt');
+              if (!response.ok) throw new Error('Could not load passwords file.');
+              const text = await response.text();
+              const validPasswords = text.split('\n').map(pw => pw.trim());
+          } else {
+              localStorage.setItem('role', username);
+              const response = await fetch('Password.txt');
+              if (!response.ok) throw new Error('Could not load passwords file.');
+              const text = await response.text();
+              const validPasswords = text.split('\n').map(pw => pw.trim());
+          }
+         if (validPasswords.includes(password)) {
+              localStorage.setItem('authenticated', 'true');
+              window.location.href = 'index_1.html';
+             } else {
+              message.textContent = 'Invalid password.';
+              message.style.color = 'red';
             }
+          
+
         } catch (error) {
             message.textContent = 'An error occurred: ' + error.message;
             message.style.color = 'red';
