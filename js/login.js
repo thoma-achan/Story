846555317b7340d0a6ad8551c25336b8b8d88cf8
thoma-achan <76838413+thoma-achan@users.noreply.github.com>
2025-01-01 17:39:19 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,25 +3,13 @@
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
         const message = document.getElementById('message');
-        fetch('https://api.ipify.org?format=json')
-           .then(response => response.json())
-           .then(data => {
-            const currentIP = data.ip;
-           })
 	     
         try {
             const response = await fetch('Password.txt');
             if (!response.ok) throw new Error('Could not load passwords file.');
             const text = await response.text();
             const validPasswords = text.split('\n').map(pw => pw.trim());
-
-            const response = await fetch('ip.txt');
-            if (!response.ok) throw new Error('Could not load passwords file.');
-
-            const text = await response.text();
-            const validip = text.split('\n').map(pw => pw.trim());
-           
-            if (validPasswords.includes(password) && validip.includes(currentIP)) {
+            if (validPasswords.includes(password)) {
                 localStorage.setItem('authenticated', 'true');
                 window.location.href = 'index_1.html';
                 localStorage.setItem('username', username);
